7594062992a7cf247bd5db82a003556700e60778
Helena DQ <delmedicolena@gmail.com>
2024-11-08 18:03:43 -0300

    Add files via upload

---
+++ A25_Construtores.kt
@@ -0,0 +1,16 @@
+// 25 : Construtores primários e secundários
+
+class vazia
+class Qualquer private constructor()
+
+class Persona(var nome: String, var anoNasci: Int){
+    var altura: Double
+    var doc: String? = null
+
+}
+fun main(){
+
+
+
+}
+
